de43479c7ab6911ceb70676ae8e884da3a26fbab
valenadels <v.adelsflugel@despegar.com>
2023-11-20 22:04:37 -0300

    acomodo repo

--- Firewall.py
+++ Firewall.py
@@ -26,6 +26,40 @@ class Firewall (EventMixin):
         if event.dpid == self.swith_id:  # Identificador del switch donde se instalarán las reglas
             self.setRules(event)
 
+    def setRules(self, event):
+        for rule in self.rules["rules"]:
+            flow_mod = self.create_flow_mod(rule)
+            event.connection.send(flow_mod)
+
+        log.debug("FIREWALL RULES INSTALLED ON SWITCH %s", dpidToStr(event.dpid))
+
+    def create_flow_mod(self, rule):
+        flow_mod = of.ofp_flow_mod()
+        flow_mod.match.dl_type = self.mapIpType(rule["ip_type"])
+
+        if "src_ip" in rule:
+            flow_mod.match.nw_src = IPAddr(rule["src_ip"])
+        if "dst_ip" in rule:
+            flow_mod.match.nw_dst = IPAddr(rule["dst_ip"])
+        if "src_port" in rule:
+            flow_mod.match.tp_src = rule["src_port"]
+        if "dst_port" in rule:
+            flow_mod.match.tp_dst = rule["dst_port"]
+        if "protocol" in rule:
+            if rule["protocol"] == "UDP":
+                flow_mod.match.nw_proto = pkt.ipv4.UDP_PROTOCOL
+            elif rule["protocol"] == "TCP":
+                flow_mod.match.nw_proto = pkt.ipv4.TCP_PROTOCOL
+
+        return flow_mod
+
+
+    def mapIpType(self, type):
+        if type == "ipv6":
+            return  pkt.ethernet.IPV6_TYPE
+        return pkt.ethernet.IP_TYPE
+
+
     def setConfiguration(self):
         file = open('config.json')
         config = json.load(file)
@@ -33,51 +67,5 @@ class Firewall (EventMixin):
         self.swith_id = config["firewall_id"]
         self.rules = config["rules"]
     
-    def setRules(self, event):
-        # Regla 1: Descartar mensajes con puerto destino 80
-        rule1_udp = of.ofp_flow_mod()
-        rule1_udp.match.tp_dst = self.rules[0]["dst_port"]  # Puerto destino 80
-        rule1_udp.match.dl_type = pkt.ethernet.IP_TYPE
-        rule1_udp.match.nw_proto = pkt.ipv4.UDP_PROTOCOL
-        event.connection.send(rule1_udp)
-
-        rule1_tcp = of.ofp_flow_mod()
-        rule1_tcp.match.tp_dst = self.rules[0]["dst_port"]  # Puerto destino 80
-        rule1_tcp.match.dl_type = self.typeIp(self.rules[0]["ip_type"])
-        rule1_tcp.match.nw_proto = pkt.ipv4.TCP_PROTOCOL
-        event.connection.send(rule1_tcp)
-        
-        # Regla 2: Descartar mensajes desde el host 1 al puerto 5001 usando UDP
-        rule2 = of.ofp_flow_mod()
-        rule2.match.dl_type = pkt.ethernet.IP_TYPE
-        if self.rules[1]["protocol"] == "UDP":
-            rule2.match.nw_proto = pkt.ipv4.UDP_PROTOCOL
-        elif self.rules[1]["protocol"] == "TCP":
-            rule2.match.nw_proto = pkt.ipv4.TCP_PROTOCOL
-        rule2.match.nw_src = IPAddr(self.rules[1]["src_ip"])  # Dirección IP del host 1
-        rule2.match.tp_dst = self.rules[1]["dst_port"]  # Puerto destino 5001
-        event.connection.send(rule2)
-
-        # Regla 3: Bloqueo de comunicacion entre 2 hosts cualquiera (bilateral).
-        rule3_1 = of.ofp_flow_mod()
-        rule3_1.match.dl_type = pkt.ethernet.IP_TYPE
-        rule3_1.match.nw_src = IPAddr(self.rules[2]["src_ip"])
-        rule3_1.match.nw_dst = IPAddr(self.rules[2]["dst_ip"])
-        event.connection.send(rule3_1)
-        
-        rule3_2 = of.ofp_flow_mod()
-        rule3_2.match.dl_type = pkt.ethernet.IP_TYPE
-        rule3_2.match.nw_src = IPAddr(self.rules[2]["dst_ip"])
-        rule3_2.match.nw_dst = IPAddr(self.rules[2]["src_ip"])
-        event.connection.send(rule3_2)
-
-        log.debug("FIREWALL RULES INSTALLED ON SWITCH %s", dpidToStr(event.dpid))
-
-    def typeIp(self, type):
-        if type == "ipv4":
-            return pkt.ethernet.IP_TYPE
-        elif type == "ipv6":
-            return  pkt.ethernet.IPV6_TYPE
-
 def launch():
     core.registerNew(Firewall)
--- topology.py
+++ topology.py
@@ -1,20 +1,24 @@
 from mininet.topo import Topo
 
+MAX_HOSTS = 4
+MIN_SWITCHES = 1
+HOST = 'h'
+SWITCH = 's'
 
 class Tp2Topo(Topo):
     def __init__(self, n_switches):
         Topo.__init__(self)
-        if n_switches < 1:
+        if n_switches < MIN_SWITCHES:
             raise Exception("Number of switches must be at least 1")
         
 
         hosts = []
-        for i in range(4):
-            hosts.append(self.addHost('h'+str(i + 1)))
+        for i in range(MAX_HOSTS):
+            hosts.append(self.addHost(HOST+str(i + 1)))
 
         switches = []
         for i in range(n_switches):
-            switches.append(self.addSwitch('s'+str(i+1)))
+            switches.append(self.addSwitch(SWITCH+str(i+1)))
 
         self.addLink(hosts[0], switches[0])
         self.addLink(hosts[1], switches[0])
@@ -22,7 +26,7 @@ class Tp2Topo(Topo):
         self.addLink(hosts[3], switches[n_switches-1])
 
         for i in range(n_switches-1):
-            print("connect swith {} with {}".format(i+1, i+2))
+            print("connect switch {} with {}".format(i+1, i+2))
             self.addLink(switches[i], switches[i+1])
 
 
